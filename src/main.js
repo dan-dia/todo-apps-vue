054fa148e805d48878cb5dbbf315083f19160f19
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2022-07-10 12:20:09 +0700

    add vuetify

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import VueCompositionAPI, { createApp, h } from '@vue/composition-api';
 
 import App from './App.vue';
-import './plugins/bootstrap-vue';
 import './plugins/sweetalert2-vue';
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueCompositionAPI);
 
 const app = createApp({
+  vuetify,
   render: () => h(App),
 });
 
